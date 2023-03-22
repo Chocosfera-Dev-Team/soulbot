3059dae6ba176884ecd5f247e4ec4df3e8e682e8
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-22 15:00:53 +0200

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
     </>
   );
 }
