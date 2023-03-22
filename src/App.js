94beaa51b0b627e94cbed7c7e4299d481a9a3307
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-22 16:05:07 +0200

    Added endorsements section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Endorsements from "./components/Endorsements";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Endorsements />
     </>
   );
 }
---
+++ src/components/Endorsements.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Endorsements() {
+  return (
+    <div className="endorsements">
+      <div className="content-container">
+        <h2>Endorsements and partners</h2>
+
+        <ul className="endorsements-list">
+          <li className="endorsement">
+            <img
+              src="https://images.unsplash.com/photo-1518135714426-c18f5ffb6f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt="partner"
+            />
+          </li>
+          <li className="endorsement">
+            <img
+              src="https://images.unsplash.com/photo-1518135714426-c18f5ffb6f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt="partner"
+            />
+          </li>
+          <li className="endorsement">
+            <img
+              src="https://images.unsplash.com/photo-1518135714426-c18f5ffb6f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt="partner"
+            />
+          </li>
+          <li className="endorsement">
+            <img
+              src="https://images.unsplash.com/photo-1518135714426-c18f5ffb6f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt="partner"
+            />
+          </li>
+          <li className="endorsement">
+            <img
+              src="https://images.unsplash.com/photo-1518135714426-c18f5ffb6f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt="partner"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
